d40272acfdbc63d07cb454e4d0da3035b4d4a671
Colin Lebens <colin.lebens@gmail.com>
2014-04-07 19:38:28 -0700

    Changes to data passed with teams; fix team name

--- updateTeams.js
+++ updateTeams.js
@@ -9,7 +9,7 @@ var xmlstatsRoot = 'https://erikberg.com/';
 var db = require('orchestrate')(orcKey);
 
 function updateTeamsFromXMLStats() {
-	db.deleteCollection('Teams');
+
 
 	superagent
 	.get(xmlstatsRoot + 'nba/teams.json')
@@ -21,11 +21,18 @@ function updateTeamsFromXMLStats() {
 			var teamId;
 			
 			teamId = xmlTeamObj['team_id'];
-			teamObject['teamName'] = xmlTeamObj['first_name'] + ' '
+			teamObject['teamName'] = xmlTeamObj['first_name'] + ' ' +
 										xmlTeamObj['last_name'];
 			teamObject['teamLogo'] = '/images/logos/' + teamId + '-logo.jpg';
-			teamObject['foodRules'] = [];
+			teamObject['foodRules'] = [{
+				'foodId': 'chalupa',
+				'foodRule': '100pts',
+				'ruleDescription': 'Completely Random; 50% chance'
+			}];
+			
 			addTeamToDB(teamId, teamObject);
+			addTeamKeyToDB(teamId);
+			
 		});
 			
 	});		
@@ -35,5 +42,38 @@ function addTeamToDB(teamId, teamObject) {
 	console.log(teamId + ': ' + JSON.stringify(teamObject));
 	db.put('Teams', teamId, teamObject); 
 }
+
+function addTeamKeyToDB(key) {
+	var success = false;
+	var intervalObj;
+
+	function dbEdit() {
+		db.get('Teams', 'keys')
+		.then (function(result) {
+			var keyList = result.body.keyList;
+			var ref = result.headers.etag;
+			
+			keyList.push(key);
+			console.log(keyList);
+			db.put('Teams', 'keys', {"keyList": keyList}, ref)
+			.then(function(result) {
+				clearInterval(intervalObj);
+				console.log(keyList);
+			})
+			.fail(function(err) {
+				//console.log(err);
+			});
+		})
+		.fail(function(err) {
+			console.log('Cant find keys');
+		});
+
+			
+	}
+
+	intervalObj = setInterval(dbEdit, Math.floor(Math.random() * 2000) + 1000);
+
+}
+
 
 updateTeamsFromXMLStats();

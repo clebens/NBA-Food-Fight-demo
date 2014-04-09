d810bd99fd1f002ee6e5333a48164455c04c4e1d
Colin Lebens <colin.lebens@gmail.com>
2014-04-08 18:08:08 -0700

    Add underscore to js/lib

---
+++ public/js/lib/underscore.js
@@ -0,0 +1,31 @@
+
+  <html><head><title>302 Redirection</title></head>
+  <body bgcolor=#ffffff>
+    <!--ServiceProvider=Wayport-->
+    <!--access procedure=WY.1-->
+    <!--access location=wp_223697.145640-->
+    <!--error=0-->
+    <!-- LoginURL=https://nmd.hil-pdxphhh.sea.wayport.net/roamer_login.adp -->
+    <!-- GIS 
+        <?xml version="1.0" encoding="UTF-8"?>
+        <WISPAccessGatewayParam 
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:noNamespaceSchemaLocation=
+                    "https://nmd.hil-pdxphhh.sea.wayport.net/WayportGISParam.xsd">
+            <Redirect>
+                <AccessProcedure>1.0</AccessProcedure>
+                <AccessLocation>wp_223697.145640</AccessLocation>
+                <LocationName>Hilton Portland &amp; Executive Tower</LocationName>
+                <AuthenticationSeed>86AAEBFAC74099A68B53811CFC4B29E2</AuthenticationSeed>
+                <LoginURL>https://nmd.hil-pdxphhh.sea.wayport.net/roamer_login.adp?MacAddr=C4%3a85%3a08%3a17%3a2B%3a3B&amp;IpAddr=192%2e168%2e7%2e140&amp;Ip6Addr=&amp;vsgpId=&amp;vsgId=543275&amp;UserAgent=&amp;ProxyHost=&amp;TunnelIfId=3365863&amp;VlanId=22</LoginURL>
+                <MessageType>100</MessageType>
+                <ResponseCode>0</ResponseCode>
+            </Redirect>
+        </WISPAccessGatewayParam>
+    End GIS -->
+    <h2>302 Found</h2>
+    <hr>
+    Click <a href="http://nmd.hil-pdxphhh.sea.wayport.net/index.adp?MacAddr=C4%3a85%3a08%3a17%3a2B%3a3B&IpAddr=192%2e168%2e7%2e140&Ip6Addr=&vsgpId=&vsgId=543275&UserAgent=&ProxyHost=&TunnelIfId=3365863&VlanId=22">here</a> 
+    to get the document you requested.
+  </body></html>
+    

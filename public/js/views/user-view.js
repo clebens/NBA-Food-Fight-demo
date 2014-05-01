edce325f95cea57a1e38eb13468a8042d5d0af7a
Colin Lebens <colin.lebens@gmail.com>
2014-04-30 17:44:50 -0700

    Pre-merge

--- public/js/views/user-view.js
+++ public/js/views/user-view.js
@@ -111,9 +111,9 @@ define(function (require) {
             $('.modal-backdrop').remove();
             $('#failed-login').html('<p class="text-success">Thanks for signing up, ' + userName + '!');
             $('#failed-login').show();
-            $('#user-name').show();
+            
             window.location.href = "#user-view";
-
+            $('#user-name').hide();
           }
         }
       });
